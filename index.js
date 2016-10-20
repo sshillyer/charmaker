4a421e9223e304497799d7cabaa94e1bce35878d
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-10-19 23:00:00 -0700

    Implement main route, /chargen

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 Author:         Shawn Hillyer
 Description:    CS 496, Oregon State University
 *******************************************************************************/
-"use strict";
+"use strict"
 
 /*******************************************************************************
  * Middleware Setup
@@ -16,9 +16,11 @@ const mongoose = require('mongoose');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
 
+// Use submodule for routes
+const routes = require('./routes');
 
 // Import body-parser / setup (middleware for parsing POST content)
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -29,15 +31,15 @@ app.use(bodyParser.json());
 
 // Import credentials (passwords etc.)
 // Note: Always make sure to add database-level user (not just an Mlab.com user, for example)
-var credentials = require('./credentials.js');
+const credentials = require('./credentials.js');
 
 
 // Require in the mongoose modules, set connection string
-var url = credentials.mongo.development.connectionString;
+const url = credentials.mongo.development.connectionString;
 
 
 // Keeps the server alive when an error occurs
-var opts = {
+const opts = {
     server: {
         socketOptions: { keepAlive: 1 } // prevent database errors (good for a web-based API/website)
     }
@@ -45,7 +47,7 @@ var opts = {
 
 // Connect to MongoLab database and start server up
 // Cite: https://zellwk.com/blog/crud-express-mongodb/
-// The db var is set here s owe have global access to it
+// The db const is set here s owe have global access to it
 var db;
 
 // Connect to MongoDB and launch app; on failure, log error and exit
@@ -54,7 +56,7 @@ mongoose.connect(credentials.mongo.development.connectionString, function(err, d
     
     db = database;  // Assign the connection to the db variable
     
-    var port = process.env.PORT || 8090;
+    const port = process.env.PORT || 8090;
     
     app.listen(port, function() {
         console.log('listening on ' + port)
@@ -69,5 +71,4 @@ app.use(function(req,res,next){
 });
 
 
-// Externalize routes into submodules
-const routes = require('./routes');
+app.use('/', routes);
--- routes.js
+++ routes.js
@@ -12,30 +12,22 @@ const router = new Router();    // Instantiate the router object
 // Import our various schemas so we can use them in the routes
 const characters = require('./models/characters-schema');
 
-router.route('/').get((req, res) => {
-  res.json({ message: 'Welcome to chargen API!' });
-});
-
-
-// Base URL route:
-//router.route('/').get((req,res) => {
-//    res.header("Access-Control-Allow-Origin", "*");
-//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//    next();
-//    
-//    res.json({ 
-//        message: 'Welcome to charmaker API!',
-//        version: "1.0",
-//    });
+//router.route('/').get((req, res) => {
+//  res.json({ message: 'Welcome to chargen API!' });
 //});
 
 
+// Base URL route: API Landing page
+router.route('/chargen').get((req,res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    
+    res.json({ 
+        message: 'Welcome to charmaker API!',
+        version: "1.0",
+    });
+});
 
-//router.get('/', function(){
-//        res.json({ 
-//        message: 'Welcome to charmaker API!',
-//        version: "1.0",
-//    });
-//})
 
 module.exports = router;
