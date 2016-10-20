9b905d4eec3e526053ec10e510ba0ca41e91ecb4
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-10-19 21:20:07 -0700

    Add index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+/*******************************************************************************
+Author:         Shawn Hillyer
+Description:    CS 496, Oregon State University
+*******************************************************************************/
+"use strict"
+
+var mongoose = require('mongoose');
+
+
+
+// Require in the mongoose modules, set connection string
+var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var url = credentials.mongo.development.connectionString;
